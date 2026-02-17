9f473ff23027adc0e2bfad335be91945a6151972
Yahya9102 <yaya_hussein@hotmail.com>
2026-02-17 18:33:14 +0100

    full crud done for user-service

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,17 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"user-service-app/internals/api"
+	"user-service-app/internals/repository"
+	"user-service-app/internals/service"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		fmt.Fprintln(w,"THE API IS WORKING")
-
-	})
+	repo := repository.NewUserRepository()
+	userService := service.NewUserService(repo)
+	handler := api.NewUserHandler(userService)
+	handler.RegisterRoutes()
 	
-	 fmt.Println("Servern running on http://localhost:8080")
+	
+
+	fmt.Println("Servern running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
--- internals/api/user_handler.go
+++ internals/api/user_handler.go
@@ -3,6 +3,8 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 	"user-service-app/internals/service"
 )
 
@@ -16,10 +18,72 @@ func NewUserHandler(s *service.UserService) *UserHandler {
 
 func (h *UserHandler) RegisterRoutes() {
 	http.HandleFunc("/users", h.handleUsers)
+	http.HandleFunc("/users/", h.handleUsersByID)
 }
 
 
+
+
+// /users/{id}
+
+
+func (h *UserHandler) handleUsersByID(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-type", "application/json")
+
+
+
+	idStr := strings.TrimPrefix(r.URL.Path, "/users/")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	switch r.Method {
+	case http.MethodDelete:
+		ok := h.userService.DeleteUser(id)
+
+		if !ok {
+			http.Error(w, "Cant find the user", http.StatusNotFound)
+			return
+		}
+
+		w.WriteHeader(http.StatusNoContent) // 204
+	
+	case http.MethodPut:
+		var input struct {
+			Name string `json:"name"`
+			Age int `json:"age"`
+		}
+
+		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+			http.Error(w, "Invalid json", http.StatusBadRequest)
+			return
+		}
+
+
+		user, ok := h.userService.UpdateUser(id, input.Name, input.Age)
+
+		if !ok {
+			http.Error(w, "Can not find the user", http.StatusNotFound)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(user)
+
+	default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	
+	}
+
+}
  
+
+
+
 func (h *UserHandler) handleUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -33,8 +97,8 @@ func (h *UserHandler) handleUsers(w http.ResponseWriter, r *http.Request) {
 	case http.MethodPost:
 		
 		var input struct {
-			Name string `json: "name"`
-			Age int `json: "age"`
+			Name string `json:"name"`
+			Age int `json:"age"`
 			
 		}  
 
@@ -55,19 +119,6 @@ func (h *UserHandler) handleUsers(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
 	}
 
-
-
-
-
 }
 
 
-
-
-/*
-func handleUSers(h *UserHandler ) (w http.ResponseWriter, r *http.Request){
-
-
-}
-
-*/
