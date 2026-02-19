1eb29b82d3405f2d82947bb7cba014caf20607bf
Yahya9102 <yaya_hussein@hotmail.com>
2026-02-19 18:42:19 +0100

    add gin code, get and post

---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Employee struct {
+	ID        int      `json:"id"`
+	Name      string   `json:"name"`
+	Role      string   `json:"role"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+
+
+var employees = []Employee{}
+
+var nextID = 1
+
+
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/employees", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, employees)
+	})
+
+
+
+	r.GET("/employees/:id", func(ctx *gin.Context) {
+
+		idParam := ctx.Param("id")
+
+		id, err := strconv.Atoi(idParam)
+
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "id must be a number",
+			})
+			return
+		}
+
+		for _, e := range employees {
+			if e.ID == id {
+				ctx.JSON(http.StatusOK, e)
+				return
+			}
+		}
+
+
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error": "employee not found",
+		})
+
+	})
+
+
+	r.POST("/employees", func(ctx *gin.Context) {
+
+		var input struct {
+			Name string `json:"name"`
+			Role string `json:"role"`
+		}
+
+
+		if err := ctx.BindJSON(&input); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid JSON",
+			})
+			return
+		}
+
+
+		if input.Name == "" || input.Role == "" {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "name and role are required",
+			})
+			return
+		}
+
+
+
+		newEmployee := Employee{
+			ID: nextID,
+			Name: input.Name,
+			Role: input.Role,
+			CreatedAt: time.Now(),
+		}
+
+		nextID++
+
+
+		employees = append(employees, newEmployee)
+
+		ctx.JSON(http.StatusCreated, newEmployee)
+
+	})
+
+	r.Run(":8080")
+
+}
